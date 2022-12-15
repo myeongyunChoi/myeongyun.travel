dd8afe204cf7fb92c5f6d7d7fc4030412dd68ed5
myeongyunChoi <104118233+myeongyunChoi@users.noreply.github.com>
2022-12-15 23:59:51 +0900

    planner section

--- src/page/Planner.jsx
+++ src/page/Planner.jsx
@@ -1,8 +1,17 @@
+import '../css/Planner.css'
 
 const Planner = () => {
 
     return (
-        <h1>asd</h1>
+        <>
+            <header>
+                <h1>MYPLANNER</h1><p>당신의 여행의 길잡이가 되어줄 플래너</p>
+            </header>
+            <div className="plan_form">
+                <div className="plan"></div>
+                <div className="search_board"></div>
+            </div>
+        </>
     )
 }
 
