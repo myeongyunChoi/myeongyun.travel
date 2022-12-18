d508dfe81e1ca31dc87e270d093d576120a49541
myeongyunChoi <104118233+myeongyunChoi@users.noreply.github.com>
2022-12-18 23:36:21 +0900

    planner api

--- src/page/Planner.jsx
+++ src/page/Planner.jsx
@@ -1,13 +1,39 @@
-import { useNavigate } from 'react-router-dom'
-import '../css/Planner.css'
+import { useNavigate } from 'react-router-dom';
+import '../css/Planner.css';
+import { useState, useEffect } from 'react';
 
 const Planner = () => {
+
+    const [allList, setAll] = useState()
+    const [listData, setList] = useState([])
+    const [listStartNum, setStart] = useState(2);
+    const [listEndNum, setEnd] = useState(6);
+    const [url, setUrl] = useState(5)
+
+    useEffect(() => {
+        const myKey = "ad66d7j6pd5i7q0m";
+        let c1 = "관광지", c2 = "쇼핑", c3 = "숙박", c4 = "음식점", c5 = "축제/행사";
+        //category=c8&
+        fetch(`https://api.visitjeju.net/vsjApi/contents/searchList?apiKey=&locale=kr&category=c${url}&`)
+            .then(res => {
+                return res.json();
+            })
+            .then(data => {
+                let copyData = [...listData];
+                for (let i = listStartNum; i < listEndNum; i++) {
+                    copyData.push(data.items[i])
+                }
+                setList(copyData);
+                console.log(url)
+            })
+            console.log(url)
+    }, [url])
 
     const navigate = useNavigate();
     return (
         <>
             <header>
-                <h1 onClick={()=>{navigate("/")}}>MYPLANNER</h1><p>당신의 여행의 길잡이가 되어줄 플래너</p>
+                <h1 onClick={() => { navigate("/") }}>MYPLANNER</h1><p>당신의 여행의 길잡이가 되어줄 플래너</p>
             </header>
             <div className="plan_form">
                 <div className="plan">
@@ -17,45 +43,52 @@ const Planner = () => {
                         <li><h2 className="day">1DAY / 3DAY</h2></li>
                         <li><p>2022. 12. 19 - 2022. 12. 21</p></li>
                     </ul>
-                    <div className="hotel">
-                        <h3>호텔 / 숙박</h3>
-                        <p className="hotel_name"></p>
-                    </div>
                     <div className="festival">
                         <h3>축제 / 행사</h3>
                         <p className="festival_name"></p>
                     </div>
                     <div className="restaurant">
-                        <h3>카페 / 음식점</h3>
+                        <h3 onClick={()=>{setUrl(4)}}>카페 / 음식점</h3>
                         <p className="restaurant_name"></p>
                     </div>
                     <div className="place">
-                        <h3>관광지</h3>
+                        <h3 onClick={()=>{setUrl(1)}}>관광지</h3>
                         <p className="place_name"></p>
                     </div>
+                    <div className="hotel">
+                        <h3 onClick={()=>{setUrl(2)}}>호텔 / 숙박</h3>
+                        <p className="hotel_name"></p>
+                    </div>
                 </div>
-                <Search_plan />
+                <Search_plan listData={listData} setUrl={setUrl}/>
             </div>
         </>
     )
 }
 
-const Search_plan = () => {
+const Search_plan = ({ listData, setUrl }) => {
     return (
         <div className="search_board">
             <input type="text" placeholder="검색어를 입력해주세요" />
             <ul className="search_category">
-                <li>호텔 / 숙박</li>
-                <li>축제 / 행사</li>
-                <li>카페 / 음식점</li>
-                <li>관광지</li>
+                <li onClick={()=>{setUrl(5)}}>축제 / 행사</li>
+                <li onClick={()=>{setUrl(4)}}>카페 / 음식점</li>
+                <li onClick={()=>{setUrl(1)}}>관광지</li>
+                <li onClick={()=>{setUrl(2)}}>호텔 / 숙박</li>
             </ul>
-            <ui className="search_result">
-                <li></li>
-                <li></li>
-                <li></li>
-                <li></li>
-            </ui>
+            <ul className="search_result">
+                {
+                    listData.map((item, idx) => {
+                        return (
+                            <li key={idx}>
+                                <img className="hotel_img" src={item.repPhoto.photoid.thumbnailpath} alt="hotel img" />
+                                <h4>{item.title}</h4> 
+                            </li>
+                            // <img src={item.repPhoto.photoId?.imgpath} alt="" />
+                        )
+                    })
+                }
+            </ul>
         </div>
     )
 }
